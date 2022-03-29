74fad0b2a3ecd9ea20db69038953c6c95818523a
Devesh Shakya <06deveshshakya@gamil.com>
2022-03-29 13:05:27 +0530

    chnaged file structure

--- server/index.js
+++ server/index.js
@@ -39,10 +39,10 @@ app.use("/api/auth/", authRoutes);
 // serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "../client", "build")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
